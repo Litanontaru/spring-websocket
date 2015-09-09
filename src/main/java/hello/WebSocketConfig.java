e5f91788188a4a6af119c7f8801aafc8edc7c17e
Litanontaru <litanontaru@yahoo.com>
2015-09-09 17:42:59 +0400

    renamed

--- src/main/java/hello/WebSocketConfig.java
+++ src/main/java/hello/WebSocketConfig.java
@@ -1,5 +1,6 @@
-package old;
+package hello;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
@@ -7,11 +8,12 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 @Configuration
+@ComponentScan
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/hello").withSockJS();
     }
 
     @Override
